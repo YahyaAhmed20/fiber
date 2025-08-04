d4ccbe0bad0c49354703b69d16047b43db488430
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-08-04 17:07:06 +0300

    s

--- project/settings.py
+++ project/settings.py
@@ -84,24 +84,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'nruAUFVaFTFlgzgctRRSGvaJmtXttcvO',
-        'HOST': 'nozomi.proxy.rlwy.net',
-        'PORT': '30059',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'nruAUFVaFTFlgzgctRRSGvaJmtXttcvO',
+#         'HOST': 'nozomi.proxy.rlwy.net',
+#         'PORT': '30059',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
