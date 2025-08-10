cca84b0cafa3cd28a0af7a91a939bd21f701f03f
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-08-10 17:10:53 +0300

    a

--- home/urls.py
+++ home/urls.py
@@ -12,7 +12,11 @@ urlpatterns = [
 
 
     path("calculate_manual_combined/", views.calculate_manual_combined, name="calculate_manual_combined"),
+    path('robots.txt', views.robots_txt, name='robots_txt'),
 
+    path('sitemap.xml', views.sitemap_xml, name='sitemap_xml'),  # ← السطر الجديد
+
+    path('googleb7c44eff2d1899d9.html', views.google_verify, name='google_verify'),   
     # path('calculate/', views.calculate_dynamic_price, name='calculate_price'),
 
 ]
