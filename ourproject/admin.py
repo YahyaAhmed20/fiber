49d22811e4b67ac5111d5f2f2d9d07bf1f33b438
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-08-19 20:31:53 +0300

    Added

--- ourproject/admin.py
+++ ourproject/admin.py
@@ -1,11 +1,10 @@
-# projects/admin.py
-
 from django.contrib import admin
 from .models import Project, ProjectImage
 
 class ProjectImageInline(admin.TabularInline):
     model = ProjectImage
     extra = 1
+    can_delete = True  # هتظهر checkbox للحذف
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
