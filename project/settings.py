8454307223ed1ebb97ea341ee262904a30a64055
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-08-22 02:36:31 +0300

    AD

--- project/settings.py
+++ project/settings.py
@@ -168,6 +168,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
