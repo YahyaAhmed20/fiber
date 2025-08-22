33710732cbb3186205ab4f16d6ec594b04615965
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-08-22 13:58:48 +0300

    Modified Cert

--- certificates/models.py
+++ certificates/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.urls import reverse
-
+from cloudinary.models import CloudinaryField
 class Certificate(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='certificates/')
+    image = CloudinaryField('certificates')
     alt_text = models.CharField(max_length=150, blank=True)
 
     def __str__(self):
         return self.title
 
 
+
+    # image = models.ImageField(upload_to='certificates/')
