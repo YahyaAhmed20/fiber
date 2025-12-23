9834c2543f7f402047bf5ace7500c6afe7edf7d4
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-12-23 20:02:16 +0200

    asd

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@olq19@%nd8t78w_wb*0z9o0eh7+7z5lq(hrm3a#97726*(7*+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "web-production-ffb34.up.railway.app",
