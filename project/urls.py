5a3e3588d1325321cc70a8a151dd8f284939edd0
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2026-03-28 11:30:10 +0200

    added edits

--- project/urls.py
+++ project/urls.py
@@ -12,7 +12,8 @@ from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('control-panel-rovanax/', admin.site.urls),
 
     # 🔥 لتغيير اللغة
     path('i18n/', include('django.conf.urls.i18n')),
